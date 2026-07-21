a65774696bb3d51e402b0c84917124394d620b88
DHRUB-SAIKIA <dhrubasaikia937@gmail.com>
2026-07-21 15:27:12 +0530

    .

--- Class 4/SciPy.py
+++ Class 4/SciPy.py
@@ -1,6 +1,9 @@
-import numpy as np 
-
-from SciPy import Sparse
+import numpy as np
+from SciPy import sparse
 
 eye = np.eye(5)
 print("NumPy array:\n{}".format(eye))
+
+sparse_matrix = sparse.csr_matrix(eye)
+
+print("\nSciPy sparse CSR matrix:\n{}".format(sparse_matrix))
